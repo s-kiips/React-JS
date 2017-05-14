b1e3d8c50bd3c2fa9aff1c67350ecfa625903024
Sudin Ranjitkar <ranjitkarsudeen14@gmail.com>
2017-05-14 21:22:37 +0545

    component lifecycle

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -13,21 +13,69 @@ export class Home extends React.Component {
         super();
         this.state = {
             age: props.initialAge,
-            status:0
+            status: 0,
+            homeLink: props.initialLinkName
         };
 
         /*this setTimeOut function is used to update the status to 1 after 3 sec*/
-        setTimeout(()=>{
+        setTimeout(() => {
             this.setState({
                 status: 1
             });
-        } ,3000)
+        }, 3000)
+        console.log("Constructor");
     }
+
+/*Component Life Cycle*/
+    componentWillMount() {
+        console.log("component will mount");
+    }
+
+    componentDidMount() {
+        console.log("Component did Mount");
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("Component will receive Props", nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("Should Component Update", nextProps, nextState);
+     /*   if (nextState.status===1){
+            return false;
+        }*/
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("Component Will Update", nextProps, nextState);
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("Component Did Mount", prevProps, prevState);
+    }
+
+    componentWillUnmount() {
+        console.log("Component WIll Unmount");
+    }
+    /*Component Life Cycle End*/
 
     /*this function is used to add the age by +3 and make live update in the browser without any update*/
     onMakeOlder() {
         this.setState({
             age: this.state.age + 3
+        });
+    }
+
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
+    }
+
+    /*this function is used to change the state of home link to the target value*/
+    onHandleChange(event) {
+        this.setState({
+            homeLink: event.target.value
         });
     }
 
@@ -43,12 +91,24 @@ export class Home extends React.Component {
                 <p>Status: {this.state.status}</p>
                 <hr/>
                 <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make Me Older!</button>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <input type="text" value={this.state.homeLink}
+                       onChange={(event) => this.onHandleChange(event)}/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
 }
 
+/**
+ * There is some difference in using React.PropTypes.string and PropTypes.string
+ * @type {{name: *, initialAge: *}}
+ */
 Home.propTypes = {
     name: PropTypes.string,
     initialAge: PropTypes.number,
+    greet: PropTypes.func,
+    initialLinkName: PropTypes.string
 };
--- src/app/index.js
+++ src/app/index.js
@@ -3,31 +3,72 @@
  */
 
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
 
 import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 import AwesomeComponent from  "./components/AwesomeComponent";
 
 class App extends React.Component {
-    render(){
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home",
+            homeMounted: true
+        };
+    }
+
+    onGreet() {
+        alert("Hello!!");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    }
+
+    onChangeHomeMounted() {
+        this.setState({
+            homeMounted: !this.state.homeMounted
+        });
+    }
+
+    render() {
+        let homeCmp = "";
+        if (this.state.homeMounted) {
+            homeCmp = (
+                <Home
+                    name={"max"}
+                    initialAge={27}
+                    greet={this.onGreet}
+                    changeLink={this.onChangeLinkName.bind(this)}
+                    initialLinkName={this.state.homeLink}
+                />
+            );
+        }
+
         return (
             <div className="container">
                 <div className="col-xs-10 col-xs-offset-1">
-                    <Header homeLink="Home"/>
+                    <Header homeLink={this.state.homeLink}/>
                 </div>
                 {/*<p>sudin Ranjitkar</p>*/}
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"max"} initialAge={27}/>
-                    </div>
+                <div className="col-xs-10 col-xs-offset-1">
+                    {homeCmp}
+                </div>
+
+                <div className="col-xs-10 col-xs-offset-1">
+                    <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>
+                </div>
                 {/*<div className="col-xs-10 col-xs-offset-1">
-                    <p>Counting the number of likes</p>
-                    <AwesomeComponent/>
-                </div>*/}
+                 <p>Counting the number of likes</p>
+                 <AwesomeComponent/>
+                 </div>*/}
             </div>
         );
     }
 }
 /*window.document.getElementById("name of class")
-* is used to render the contents of app class in the browser*/
-render(<App/>,window.document.getElementById("app"));
+ * is used to render the contents of app class in the browser*/
+render(<App/>, window.document.getElementById("app"));
